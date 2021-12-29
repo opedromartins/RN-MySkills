e095628b0045f9aaabe50d3d5f4f1eb5b6ce3d7e
pmartinsb <pedro.martins+github@meliuz.com.br>
2021-12-29 20:41:12 -0300

    Add delete function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,6 +28,12 @@ export function Home(){
         setMySkills(oldState => [...oldState, data]);
     }
 
+    function handleRemoveSkill(id: string){
+        setMySkills(oldState => oldState.filter(
+            skill => skill.id !== id
+        ))
+    }
+
     useEffect(() => {
         const currentHour = new Date().getHours();
 
@@ -69,7 +75,10 @@ export function Home(){
             data={mySkills}
             keyExtractor={item => item.id}
             renderItem={({ item }) => (
-                <SkillCard skill={item.name}/>
+                <SkillCard
+                    skill={item.name}
+                    onPress={() => handleRemoveSkill(item.id)}
+                />
             )}
         />
       </View>
